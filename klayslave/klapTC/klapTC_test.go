b340ac7cd1491f5b10bdbd810985feef5f1fadf4
yumiel.ko <yumiel.ko@krustuniverse.com>
2022-09-15 17:47:15 +0900

    fix klapTC_test.go

--- klayslave/klapTC/klapTC_test.go
+++ klayslave/klapTC/klapTC_test.go
@@ -4,26 +4,15 @@ import (
 	"fmt"
 	"testing"
 
-<<<<<<< HEAD
-	"github.com/klaytn/klaytn/common"
-)
-
-func TestHowmuch(t *testing.T) {
-	fmt.Println(len(Klap_account_list))
-	var data []byte
-	data = common.FromHex("0x976fafc500000000000000000000000078b6adde60a9181c1889913d31906bbf5c3795dd")
-	fmt.Println(data)
-=======
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHowmuch(t *testing.T) {
-	fmt.Println("klap_account_list length:", len(klap_account_list))
-	fmt.Println("dataList length:", len(dataList))
-	fmt.Println("contractAddrList length:", len(contractAddrList))
-	assert.Equal(t, len(dataList), len(contractAddrList))
-	fmt.Println("dataListAdditional length:", len(dataListAdditional))
-	fmt.Println("contractAddrListAdditional length:", len(contractAddrListAdditional))
-	assert.Equal(t, len(dataListAdditional),len(contractAddrListAdditional))
->>>>>>> e3713d5 (add klapAppCallAdditionalTC)
+	fmt.Println("klap_account_list length:", len(Klap_account_list))
+	fmt.Println("dataList length:", len(DataList))
+	fmt.Println("contractAddrList length:", len(ContractAddrList))
+	assert.Equal(t, len(DataList), len(ContractAddrList))
+	fmt.Println("dataListAdditional length:", len(DataListAdditional))
+	fmt.Println("contractAddrListAdditional length:", len(ContractAddrListAdditional))
+	assert.Equal(t, len(DataListAdditional),len(ContractAddrListAdditional))
 }
